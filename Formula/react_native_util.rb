f3822ec3b8276a739e89ecb5f86617388cc07d9b
Jimmy Dee <jgvdthree@gmail.com>
2019-05-09 21:08:53 -0700

    Added react_native_util formula

---
+++ Formula/react_native_util.rb
@@ -0,0 +1,22 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class ReactNativeUtil < Formula
+  desc "Community utility CLI for React Native projects"
+  homepage "https://github.com/jdee/react_native_util"
+  url "https://github.com/jdee/react_native_util/archive/v0.1.0.tar.gz"
+  sha256 "a72431affe744d86dce79696ae8ff8afa225b95a2f5c8811b1a62e7b81d60e5e"
+
+  def install
+    ENV["GEM_HOME"] = libexec
+    system "gem", "build", "react_native_util.gemspec"
+    system "gem", "install", "react_native_util-#{version}.gem"
+    bin.install libexec/"bin/react_native_util", libexec/"bin/rn"
+    bin.env_script_all_files(libexec/"bin", :GEM_HOME => ENV["GEM_HOME"], :REACT_NATIVE_UTIL_INSTALLED_FROM_HOMEBREW => "true")
+  end
+
+  test do
+    system "#{bin}/rn", "-h"
+  end
+end
